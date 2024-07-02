dc369f82f511916c207516a33a80b8b15220efe2
Laxman Rathod <luckyrathod.dev@gmail.com>
2024-07-02 21:48:10 +0530

    feat(home): Add home page with carousels and footer
    This commit adds a new home page with the following features:
    
    Message carousels for displaying important announcements or promotional content.
    A footer section for displaying essential links and information.
    The home page is designed to provide a welcoming and informative experience for visitors. The carousels add visual interest and highlight important information, while the footer provides easy access to essential resources.

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,3 +1,58 @@
-export default function Home() {
-  return <main>Home page</main>;
-}
+"use client";
+import React from "react";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import messages from "@/messages.json";
+import Autoplay from "embla-carousel-autoplay";
+
+const Home = () => {
+  return (
+    <>
+      <main className="flex-grow flex flex-col items-center justify-center px-4 md:px-24 py-12">
+        <section className="text-center mb-8 md:mb-12">
+          <h1 className="text-3xl md:text-5xl font-bold">
+            Dive into the world of Anonymous Conversations.
+          </h1>
+          <p className="mt-3 md:mt-4 text-base">
+            Explore Anonymous Message - Where your identity remains a secret.
+          </p>
+        </section>
+        <Carousel
+          plugins={[Autoplay({ delay: 2000 })]}
+          className="w-full max-w-xs"
+        >
+          <CarouselContent>
+            {messages.map((message, index) => (
+              <CarouselItem key={index}>
+                <div className="p-1">
+                  <Card>
+                    <CardHeader>{message.title}</CardHeader>
+                    <CardContent className="flex aspect-square items-center justify-center p-6">
+                      <span className="text-lg font-semibold">
+                        {message.content}
+                      </span>
+                    </CardContent>
+                  </Card>
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      </main>
+
+      <footer className="text-center p-4 md:p-6 bg-gray-900 text-white">
+        © 2023 True Feedback. All rights reserved.
+      </footer>
+    </>
+  );
+};
+
+export default Home;
