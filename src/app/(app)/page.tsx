a32a73c42f230f86b4469b9fa29ac49d1e67edd7
Laxman Rathod <luckyrathod.dev@gmail.com>
2024-07-05 17:15:36 +0530

    feat: add server configuration

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Mail } from "lucide-react"; // Assuming you have an icon for messages
+import { Mail } from "lucide-react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import Autoplay from "embla-carousel-autoplay";
 import messages from "@/messages.json";
