fc668156d5361868a88d15bf880e7dc0e3eafe49
Laxman Rathod <luckyrathod.dev@gmail.com>
2024-07-05 13:48:12 +0530

    feat: Resolve dynamic server usage error for check-username-unique route - Refactor src/app/api/check-username-unique/route.ts - Use URL and URLSearchParams to parse query parameters - Avoid using request.url during static rendering - Ensure compatibility with Next.js static site generation

--- src/app/api/check-username-unique/route.ts
+++ src/app/api/check-username-unique/route.ts
@@ -11,15 +11,15 @@ export async function GET(request: Request) {
   await dbConnect();
 
   try {
-    const url = new URL(request.url);
-    const Username = url.searchParams.get("username");
-
-    const result = UsernameQuerySchema.safeParse(Username);
-
     // const { searchParams } = new URL(request.url);
     // const queryParams = {
     //   username: searchParams.get("username"),
     // } ;
+
+    const url = new URL(request.url);
+    const Username = url.searchParams.get("username");
+
+    const result = UsernameQuerySchema.safeParse(Username);
 
     if (!result.success) {
       const usernameErrors = result.error.format().username?._errors || [];
