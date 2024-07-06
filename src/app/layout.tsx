9a64b6f3571495844064458934485e185bf94681
Laxman Rathod <luckyrathod.dev@gmail.com>
2024-07-06 12:51:51 +0530

    feat: fix problems in app/layout file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Anonymous Message - Tell them what you really think. Anonymously. 😉",
   description:
-    "Send anonymous messages to anyone - confessions, jokes, feedback - the possibilities are endless! 😉 Sign up is quick & easy, and your identity stays hidden.🤫 Start messaging anonymously now! ➡️ [App Link]",
+    "Send anonymous messages to anyone - confessions, jokes, feedback - the possibilities are endless! 😉 Sign up is quick & easy, and your identity stays hidden.🤫 Start messaging anonymously now! ➡️",
 };
 
 export default function RootLayout({
